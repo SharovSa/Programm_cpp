08b21933a9f9ecf3da72fded76ef7a62430a1872
SharovSa <skygoldrim@gmail.com>
2023-02-23 13:36:21 +0500

    Update adapter.hpp

--- tasks/task1/adapter.hpp
+++ tasks/task1/adapter.hpp
@@ -1,5 +1,21 @@
 #include "some_library.hpp"
 
+typedef int SomeLibrary::ValueHolder::* mem_ptr_t;
+
+mem_ptr_t global_mem_ptr;
+
+template<mem_ptr_t P>
+class Transform {
+  static bool tmp;
+};
+
+template<mem_ptr_t P>
+bool Transform<P>::tmp = (global_mem_ptr = P, false);
+
+template
+class Transform<&SomeLibrary::ValueHolder::value_>;
+
 inline void SetValue(SomeLibrary::ValueHolder& valueHolder, int value) {
-    // Your code goes here
+  int &val = valueHolder.*global_mem_ptr;
+  val = value;
 }
